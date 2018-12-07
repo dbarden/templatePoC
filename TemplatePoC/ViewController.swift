7891031986e10b7864d9be255000fb87e7b0374e
Daniel Barden <daniel.barden@mhp.com>
2018-12-07 06:44:40 +0100

    Initial Commit

---
+++ TemplatePoC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TemplatePoC
+//
+//  Created by Daniel Barden on 07.12.18.
+//  Copyright © 2018 Daniel Barden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TemplatePoCTests/TemplatePoCTests.swift
@@ -0,0 +1,34 @@
+//
+//  TemplatePoCTests.swift
+//  TemplatePoCTests
+//
+//  Created by Daniel Barden on 07.12.18.
+//  Copyright © 2018 Daniel Barden. All rights reserved.
+//
+
+import XCTest
+@testable import TemplatePoC
+
+class TemplatePoCTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
